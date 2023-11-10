d16b3712bf0eb5fe00ed13e2207f7f48e0fd4c36
necro_txilok <necro_txilok@hotmail.com>
2023-11-10 15:22:24 +0100

    Adding Storybook Theme Manager

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,5 @@
-/** @type { import('@storybook/react').Preview } */
+import { themes } from '@storybook/theming'
+
 const preview = {
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
@@ -8,7 +9,10 @@ const preview = {
         date: /Date$/i,
       },
     },
+    docs: {
+      theme: themes.dark,
+    },    
   },
-};
+}
 
-export default preview;
+export default preview
