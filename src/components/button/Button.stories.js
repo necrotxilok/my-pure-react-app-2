536db16fbeb4556c3f766bf98d97857499d7e2cb
necro_txilok <necro_txilok@hotmail.com>
2023-11-17 14:14:39 +0100

    Recover Button Story in correct path

---
+++ src/components/button/Button.stories.js
@@ -0,0 +1,16 @@
+import Button from "./button"
+
+export default {
+	title: 'Components/Button',
+	component: Button,
+	parameters: {
+		layout: 'centered',
+	},
+	tags: ['autodocs'],
+};
+
+export const Basic = {
+	args: {
+		label: 'Button',
+	},
+};
